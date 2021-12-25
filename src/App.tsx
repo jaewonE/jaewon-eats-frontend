842e6c1e75b428e9841c0279a781f631457ca2ff
jaewonE <importjaewonE@gmail.com>
2021-12-26 02:38:23 +0900

    v1.0.1 Tailwind css setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+      <h1 className="text-7xl text-white font-bold">Hello World!</h1>
     </div>
   );
 }
